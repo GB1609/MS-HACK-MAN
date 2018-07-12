8d102217fa217cf772cc2b0138c5ac702d8e1084
WOFLAN\Elena <mastriaelena@gmail.com>
2018-07-12 08:45:47 +0200

    First Commit! DSL!!

--- src/darkMind_ms_hack_man.cpp
+++ src/darkMind_ms_hack_man.cpp
@@ -12,8 +12,7 @@ int width = -1;
 int height = -1;
 int numNodes = -1;
 ////////// let a pair of coordinates returns the corresponding node
-int pairToNode(const int & r, const int & c)
-{
+int pairToNode(const int & r, const int & c) {
 	return r * width + c;
 }
 
@@ -27,101 +26,82 @@ int getObjectiveGreenBugsTR(const int & posBegin);
 int getObjectiveYellowBugsBR(const int& posBegin);
 int getObjectiveBlueBugsBL(const int& posBegin);
 
-class Point;
-class Point
-{
-	public:
-		int x;
-		int y;
-		int node;
-		Point() // @suppress("Class members should be properly initialized")
-		{
-		}
-		Point(int x, int y) :
-				x(x), y(y), node(pairToNode(y, x))
-		{
-		}
-		Point(int node) :
-				x(node - ((node / width) * width)), y(node / width), node(node)
-		{
-		}
+class Point {
+public:
+	int x;
+	int y;
+	int node;
+	Point() // @suppress("Class members should be properly initialized")
+	{
+	}
+	Point(int x, int y) :
+			x(x), y(y), node(pairToNode(y, x)) {
+	}
+	Point(int node) :
+			x(node - ((node / width) * width)), y(node / width), node(node) {
+	}
 
-		Point(const Point& p) :
-				x(p.x), y(p.y), node(p.node)
-		{
-		}
-		;
-		Point(Point&& p) :
-				x(std::move(p.x)), y(std::move(p.y)), node(std::move(p.node))
-		{
-		}
-		;
-		bool operator<(const Point &a) const
-		{
-			return x < a.x || (x == a.y && y < a.y);
-		}
-		bool operator==(const Point &a) const
-		{
-			return x == a.x && y == a.y;
-		}
+	Point(const Point& p) :
+			x(p.x), y(p.y), node(p.node) {
+	}
+
+	Point(Point&& p) :
+			x(std::move(p.x)), y(std::move(p.y)), node(std::move(p.node)) {
+	}
+
+	bool operator<(const Point &a) const {
+		return x < a.x || (x == a.y && y < a.y);
+	}
+	bool operator==(const Point &a) const {
+		return x == a.x && y == a.y;
+	}
 
 };
 ///////// let a node returns its coordinates
-Point nodeToPair(const int & node)
-{
+Point nodeToPair(const int & node) {
 	int r = node / width;
 	int c = node - (r * width);
 	return std::move(Point(c, r));
 }
-class Player : public Point
-{
-	public:
-		string name;
-		string direction;
-		int id;
-		int snippets;
-		int bombs;
+class Player: public Point {
+public:
+	string name;
+	string direction;
+	int id;
+	int snippets;
+	int bombs;
 };
-class SimpleObject : public Point
-{
-	public:
-		SimpleObject(int x, int y) :
-				Point(x, y)
-		{
-		}
+class SimpleObject: public Point {
+public:
+	SimpleObject(int x, int y) :
+			Point(x, y) {
+	}
 
-		SimpleObject(int node) :
-				Point(node)
-		{
-		}
+	SimpleObject(int node) :
+			Point(node) {
+	}
 };
-class ObjectWithRounds : public Point
-{
-	public:
-		int rounds;
-		ObjectWithRounds(int x, int y, int rounds) :
-				Point(x, y), rounds(rounds)
-		{
-		}
-		ObjectWithRounds(int node, int rounds) :
-				Point(node), rounds(rounds)
-		{
-		}
-		ObjectWithRounds(int node) :
-				Point(node), rounds(-1)
-		{
-		}
+class ObjectWithRounds: public Point {
+public:
+	int rounds_or_type;
+	ObjectWithRounds(int x, int y, int rounds) :
+			Point(x, y), rounds_or_type(rounds) {
+	}
+	ObjectWithRounds(int node, int rounds) :
+			Point(node), rounds_or_type(rounds) {
+	}
+	ObjectWithRounds(int node) :
+			Point(node), rounds_or_type(-1) {
+	}
 };
 //class Cell: public Point{
 template<char delimiter>
-class StringDelimitedBy : public string
-{
+class StringDelimitedBy: public string {
 
-		friend std::istream& operator>>(std::istream& is, StringDelimitedBy& output)
-		{
-			std::getline(is, output, delimiter);
-			return is;
-		}
+	friend std::istream& operator>>(std::istream& is, StringDelimitedBy& output) {
+		std::getline(is, output, delimiter);
+		return is;
+	}
 };
 //vector<vector<bool> > is_wall;
 int timebank;
@@ -146,20 +126,15 @@ bool isSetWalls = false;
 int dx[4] = { 0, -1, 0, 1 };
 int dy[4] = { -1, 0, 1, 0 };
 string moves[4] = { "up", "left", "down", "right" };
-void initAdiacents()
-{
-	for (int r = 0; r < height; r++)
-	{
-		for (int c = 0; c < width; c++)
-		{
+void initAdiacents() {
+	for (int r = 0; r < height; r++) {
+		for (int c = 0; c < width; c++) {
 			int currentNode = pairToNode(r, c);
 			int adiacentNode;
-			for (int i = 0; i < 4; i++)
-			{
+			for (int i = 0; i < 4; i++) {
 				int adiacentR = r + dy[i];
 				int adiacentC = c + dx[i];
-				if (adiacentR < height && adiacentR >= 0 && adiacentC < width && adiacentC >= 0)
-				{
+				if (adiacentR < height && adiacentR >= 0 && adiacentC < width && adiacentC >= 0) {
 					adiacentNode = pairToNode(adiacentR, adiacentC);
 					matrixAdiacents[currentNode][adiacentNode] = 1;
 					matrixAdiacents[adiacentNode][currentNode] = 1;
@@ -168,183 +143,141 @@ void initAdiacents()
 		}
 	}
 }
-void removeAdiacents(int cell)
-{
+void removeAdiacents(int cell) {
 	Point cellCoordinate(cell);
-	for (int i = 0; i < 4; i++)
-	{
+	for (int i = 0; i < 4; i++) {
 		int adiacentR = cellCoordinate.y + dy[i];
 		int adiacentC = cellCoordinate.x + dx[i];
 
-		if (adiacentR < height && adiacentR >= 0 && adiacentC < width && adiacentC >= 0)
-		{
+		if (adiacentR < height && adiacentR >= 0 && adiacentC < width && adiacentC >= 0) {
 			int adiacentNode = pairToNode(adiacentR, adiacentC);
 			matrixAdiacents[cell][adiacentNode] = 0;
 			matrixAdiacents[adiacentNode][cell] = 0;
 		}
 	}
 }
-void parseSingleCharacter(const char& c, const int & cell)
-{
-	switch (c)
-	{
-		case 'S':
-			spawn_points.push_back(ObjectWithRounds(cell));
-			break;
-		case 'C':
-			snippets.push_back(SimpleObject(cell));
-			break;
-		case 'B':
-			weapons.push_back(ObjectWithRounds(cell));
-			break;
-		default:
-			break;
+void parseSingleCharacter(const char& c, const int & cell) {
+	switch (c) {
+	case 'S':
+		spawn_points.push_back(ObjectWithRounds(cell));
+		break;
+	case 'C':
+		snippets.push_back(SimpleObject(cell));
+		break;
+	case 'B':
+		weapons.push_back(ObjectWithRounds(cell));
+		break;
+	default:
+		break;
 	}
 }
-void parseObjects(const string & objList, const int &cell)
-{
+void parseObjects(const string & objList, const int &cell) {
 	Point currentCoordinates(nodeToPair(cell));
-	if (objList.size() == 1)
-		parseSingleCharacter(objList[0], cell);
-	else
-	{
+	if (objList.size() == 1) parseSingleCharacter(objList[0], cell);
+	else {
 		std::istringstream issl(objList);
-		std::vector<string> objs((std::istream_iterator<StringDelimitedBy<';'>>(issl)),
-				std::istream_iterator<StringDelimitedBy<';'>>());
-		for (unsigned int i = 0; i < objs.size(); i++)
-		{
-			if (objs[i].size() == 1)
-				parseSingleCharacter(objs[i][0], cell);
-			else
-			{
-				switch (objs[i][0])
-				{
-					case 'P':
-					{
-						int id = objs[i][1] - '0';
-						if (id == darkMind.id)
-						{
-							darkMind.x = currentCoordinates.x;
-							darkMind.y = currentCoordinates.y;
-						}
-						else
-						{
-							enemy.x = currentCoordinates.x;
-							enemy.y = currentCoordinates.y;
-						}
+		std::vector<string> objs((std::istream_iterator<StringDelimitedBy<';'>>(issl)), std::istream_iterator<StringDelimitedBy<';'>>());
+		for (unsigned int i = 0; i < objs.size(); i++) {
+			if (objs[i].size() == 1) parseSingleCharacter(objs[i][0], cell);
+			else {
+				switch (objs[i][0]) {
+				case 'P': {
+					int id = objs[i][1] - '0';
+					if (id == darkMind.id) {
+						darkMind.x = currentCoordinates.x;
+						darkMind.y = currentCoordinates.y;
+					} else {
+						enemy.x = currentCoordinates.x;
+						enemy.y = currentCoordinates.y;
 					}
+				}
+					break;
+				case 'S': {
+					spawn_points.push_back(ObjectWithRounds(cell, stoi(objs[i].erase(0, 1))));
+				}
+					break;
+				case 'G':
+					int d;
+					switch (objs[i][1]) {
+					case 'u':
+						d = 0;
 						break;
-					case 'S':
-					{
-						spawn_points.push_back(ObjectWithRounds(cell, stoi(objs[i].erase(0, 1))));
+					case 'd':
+						d = 1;
+						break;
+					case 'l':
+						d = 2;
+						break;
+					case 'r':
+						d = 3;
+						break;
 					}
-						break;
-					case 'G':
-						int d;
-						switch (objs[i][1])
-						{
-							case 'u':
-								d = 0;
-								break;
-							case 'd':
-								d = 1;
-								break;
-							case 'l':
-								d = 2;
-								break;
-							case 'r':
-								d = 3;
-								break;
-						}
-						gates.push_back(ObjectWithRounds(cell, d));
-						break;
-					case 'E':
-						bugs.push_back(ObjectWithRounds(cell, objs[i][1] - '0'));
-						break;
-					case 'B':
-						weapons.push_back(ObjectWithRounds(cell, stoi(objs[i].erase(0, 1))));
-						break;
-					default:
-						break;
+					gates.push_back(ObjectWithRounds(cell, d));
+					break;
+				case 'E':
+					bugs.push_back(ObjectWithRounds(cell, objs[i][1] - '0'));
+					break;
+				case 'B':
+					weapons.push_back(ObjectWithRounds(cell, stoi(objs[i].erase(0, 1))));
+					break;
+				default:
+					break;
 				}
 			}
 		}
 	}
 }
-void addAdiacentsGates()
-{
+void addAdiacentsGates() {
 	for (unsigned int i = 0; i < gates.size(); i++)
 		for (unsigned int j = 0; j < gates.size(); j++)
 			matrixAdiacents[gates[i].node][gates[j].node] = 1;
 
 }
-void process_next_command()
-{
+void process_next_command() {
 	string command;
 	cin >> command;
-	if (command == "settings")
-	{
+	if (command == "settings") {
 		string type;
 		cin >> type;
-		if (type == "timebank")
-		{
+		if (type == "timebank") {
 			cin >> timebank;
-		}
-		else if (type == "time_per_move")
-		{
+		} else if (type == "time_per_move") {
 			cin >> time_per_move;
-		}
-		else if (type == "player_names")
-		{
+		} else if (type == "player_names") {
 			string names;
 			cin >> names;
 			// player names aren't very useful
-		}
-		else if (type == "your_bot")
-		{
+		} else if (type == "your_bot") {
 			cin >> darkMind.name;
-		}
-		else if (type == "your_botid")
-		{
+		} else if (type == "your_botid") {
 			cin >> darkMind.id;
-		}
-		else if (type == "field_width")
-		{
+		} else if (type == "field_width") {
 			cin >> width;
-		}
-		else if (type == "field_height")
-		{
+		} else if (type == "field_height") {
 			cin >> height;
-		}
-		else if (type == "max_rounds")
-		{
+		} else if (type == "max_rounds") {
 			cin >> max_rounds;
 		}
-		if (width != -1 && height != -1)
-		{
+		if (width != -1 && height != -1) {
 			//int tmp=width;
 			//width=height;
 			//height=tmp;
 			numNodes = width * height;
 			matrixAdiacents = new bool*[numNodes];
-			for (int r = 0; r < numNodes; r++)
-			{
+			for (int r = 0; r < numNodes; r++) {
 				matrixAdiacents[r] = new bool[numNodes] { 0 };
 			}
 			initAdiacents();
 		}
 
 	}
-	else if (command == "update")
-	{
+
+	else if (command == "update") {
 		string player_name, type;
 		cin >> player_name >> type;
-		if (type == "round")
-		{
+		if (type == "round") {
 			cin >> current_round;
-		}
-		else if (type == "field")
-		{
+		} else if (type == "field") {
 			snippets.clear();
 			weapons.clear();
 			bugs.clear();
@@ -354,75 +287,53 @@ void process_next_command()
 			cin >> s;
 
 			std::istringstream iss(s);
-			std::vector<std::string> fields((std::istream_iterator<StringDelimitedBy<','>>(iss)),
-					std::istream_iterator<StringDelimitedBy<','>>());
+			std::vector<std::string> fields((std::istream_iterator<StringDelimitedBy<','>>(iss)), std::istream_iterator<StringDelimitedBy<','>>());
 
-			for (unsigned int ce = 0; ce < fields.size(); ce++)
-			{
-				if (fields[ce][0] == 'x' && !isSetWalls)
-					removeAdiacents(ce);
-				else if (fields[ce][0] == 'x')
-					continue;
+			for (unsigned int ce = 0; ce < fields.size(); ce++) {
+				if (fields[ce][0] == 'x' && !isSetWalls) removeAdiacents(ce);
+				else if (fields[ce][0] == 'x') continue;
 				parseObjects(fields[ce], ce);
 			}
 			if (!isSetWalls) //todo delete if the gates modify their direction
 				addAdiacentsGates();
 			isSetWalls = true;
-		}
-		else if (type == "snippets")
-		{
-			if (player_name == darkMind.name)
-			{
+		} else if (type == "snippets") {
+			if (player_name == darkMind.name) {
 				cin >> darkMind.snippets;
-			}
-			else
-			{
+			} else {
 				cin >> enemy.snippets;
 			}
-		}
-		else if (type == "bombs")
-		{
-			if (player_name == darkMind.name)
-			{
+		} else if (type == "bombs") {
+			if (player_name == darkMind.name) {
 				cin >> darkMind.bombs;
-			}
-			else
-			{
+			} else {
 				cin >> enemy.bombs;
 			}
 		}
-	}
-	else if (command == "action")
-	{
+	} else if (command == "action") {
 		string action;
 		cin >> action;
-		if (action == "character")
-		{
+		if (action == "character") {
 			cin >> time_remaining;
 			choose_character();
-		}
-		else if (action == "move")
-		{
+		} else if (action == "move") {
 			cin >> time_remaining;
 			do_move();
 		}
 	}
 }
 
-int main()
-{
-	while (true)
-	{
+int main() {
+	while (true) {
 		process_next_command();
 	}
-	for (int i = 0; i < numNodes; i++)
-	{
+	for (int i = 0; i < numNodes; i++) {
 		delete[] matrixAdiacents[i];
 	}
-	for (int i = 0; i < numNodes; i++)
-	{
+	for (int i = 0; i < numNodes; i++) {
 		delete matrixAdiacents[i];
 	}
+	delete matrixAdiacents;
 	return 0;
 }
 
@@ -430,37 +341,30 @@ int main()
 //  Improve the code below to win 'em all  //
 //-----------------------------------------//
 
-void choose_character()
-{
+void choose_character() {
 	cout << "bixiette" << endl;
 }
 
-void do_move()
-{
+void do_move() {
 	srand(time(NULL));
 	vector<string> valid_moves;
 	valid_moves.clear();
-	for (int dir = 0; dir < 4; dir++)
-	{
+	for (int dir = 0; dir < 4; dir++) {
 		int nextx = darkMind.x + dx[dir];
 		int nexty = darkMind.y + dy[dir];
-		if (nextx >= 0 && nextx < width && nexty >= 0 && nexty < height)
-		{
-			if (matrixAdiacents[pairToNode(darkMind.y, darkMind.x)][pairToNode(nexty, nextx)] == 1)
-			{
+		if (nextx >= 0 && nextx < width && nexty >= 0 && nexty < height) {
+			if (matrixAdiacents[pairToNode(darkMind.y, darkMind.x)][pairToNode(nexty, nextx)] == 1) {
 				valid_moves.push_back(moves[dir]);
 			}
 		}
 	}
-	if (valid_moves.size() == 0)
-	{
+	if (valid_moves.size() == 0) {
 		valid_moves.push_back("pass");
 	}
 	cout << valid_moves[rand() % valid_moves.size()] << endl;
 }
 
-float euclidianDistanceNode(const int & node1, const int & node2)
-{
+float euclidianDistanceNode(const int & node1, const int & node2) {
 	Point p1 = nodeToPair(node1);
 	Point p2 = nodeToPair(node2);
 	int sum1 = p1.x - p2.x;
@@ -468,104 +372,80 @@ float euclidianDistanceNode(const int & node1, const int & node2)
 	cout << sum1 << "       " << sum2 << endl;
 	return sqrt(pow(sum1, 2) + pow(sum2, 2));
 }
-float euclidianDistanceCells(const int & x1, const int & y1, const int & x2, const int & y2)
-{
+float euclidianDistanceCells(const int & x1, const int & y1, const int & x2, const int & y2) {
 	int sum1 = x1 - x2;
 	int sum2 = y1 - y2;
 	cout << sum1 << "       " << sum2 << endl;
 	return sqrt(pow(sum1, 2) + pow(sum2, 2));
 }
-int getClosestPlayer(const int & posBegin)
-{
+int getClosestPlayer(const int & posBegin) {
 	int closest = -1;
 	float min = width * height; //nRighe*colonne, la distanza non potra mai essere maggiore
-	for (int i = 0; i < 2; i++)
-	{
+	for (int i = 0; i < 2; i++) {
 		int posP = (i == darkMind.id) ? darkMind.node : enemy.node;
 		float dist = euclidianDistanceNode(posBegin, posP);
-		if (min > dist)
-		{
+		if (min > dist) {
 			min = dist;
 			closest = i;
 		}
 	}
 	return closest;
 }
-int getClosestBug(const int & posBegin)
-{
+int getClosestBug(const int & posBegin) {
 	int closest = -1;
 	float min = width * height; //nRighe*colonne, la distanza non potra mai essere maggiore
-	for (unsigned int i = 0; i < bugs.size(); i++)
-	{
+	for (unsigned int i = 0; i < bugs.size(); i++) {
 		int posBugs = bugs[i].node;
 		float dist = euclidianDistanceNode(posBegin, posBugs);
-		if (min > dist)
-		{
+		if (min > dist) {
 			min = dist;
 			closest = i;
 		}
 	}
 	return closest;
 }
-int getFartherPlayer(const int & posBegin)
-{
+int getFartherPlayer(const int & posBegin) {
 	int farther = -1;
 	float max = -1;
-	for (int i = 0; i < 2; i++)
-	{
+	for (int i = 0; i < 2; i++) {
 		int posP = (i == darkMind.id) ? darkMind.node : enemy.node;
 		float dist = euclidianDistanceNode(posBegin, posP);
-		if (max < dist)
-		{
+		if (max < dist) {
 			max = dist;
 			farther = i;
 		}
 	}
 	return farther;
 }
-int getObjectiveRedBugsTL(const int & posBegin)
-{
+int getObjectiveRedBugsTL(const int & posBegin) {
 	int closestPlayer = getClosestPlayer(posBegin);
-	if (closestPlayer == darkMind.id)
-		return darkMind.node;
+	if (closestPlayer == darkMind.id) return darkMind.node;
 	else return enemy.node;
 }
-int getObjectiveGreenBugsTR(const int & posBegin)
-{
+int getObjectiveGreenBugsTR(const int & posBegin) {
 	int closestPlayer = getClosestPlayer(posBegin);
 	Player p = (closestPlayer == darkMind.id) ? darkMind : enemy;
-	if (p.direction == "up")
-	{
+	if (p.direction == "up") {
 		int xNew = p.y - 4;
-		if (xNew < 0)
-			xNew = 0;
+		if (xNew < 0) xNew = 0;
 		return pairToNode(xNew, p.x);
-	}
-	else if (p.direction == "down")
-	{
+		cout << "push" << endl;
+	} else if (p.direction == "down") {
 		int xNew = p.y + 4;
-		if (xNew > height - 1)
-			xNew = height - 1;
+		if (xNew > height - 1) xNew = height - 1;
 		return pairToNode(xNew, p.x);
-	}
-	else if (p.direction == "left")
-	{
+	} else if (p.direction == "left") {
 		int yNew = p.x - 4;
-		if (yNew < 0)
-			yNew = 0;
+		if (yNew < 0) yNew = 0;
 		return pairToNode(p.y, yNew);
-	}
-	else if (p.direction == "right")
-	{
+	} else if (p.direction == "right") {
 		int yNew = p.x + 4;
-		if (yNew > width - 1)
-			yNew = width - 1;
+		if (yNew > width - 1) yNew = width - 1;
 		return pairToNode(p.y, yNew);
 	}
 	return 0;
 }
-int getObjectiveYellowBugsBR(const int& posBegin)
-{
+int getObjectiveYellowBugsBR(const int& posBegin) {
 //	int closestPlayer=getClosestPlayer(nrPlayer,posBegin);
 //	int closestBugToClPL=getClosestBug(nrBugs,closestPlayer);
 //	int xVett=abs(players[closestPlayer].x-bugs[closestBugToClPL].x);
@@ -573,11 +453,9 @@ int getObjectiveYellowBugsBR(const int& posBegin)
 //	return pairToNode(yVett*2,xVett*2);
 	return 3;
 }
-int getObjectiveBlueBugsBL(const int& posBegin)
-{
+int getObjectiveBlueBugsBL(const int& posBegin) {
 	int fartherPlayer = getFartherPlayer(posBegin);
-	if (fartherPlayer == darkMind.id)
-		return darkMind.node;
+	if (fartherPlayer == darkMind.id) return darkMind.node;
 	else return enemy.node;
 	return 4;
 }
